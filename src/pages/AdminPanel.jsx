70e2d2234cade97900d26b97a1716859360ddca3
木村善幸 <kyodajapan@gmail.com>
2026-01-21 08:31:29 +0000

    File changes

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -833,7 +833,7 @@ export default function AdminPanel() {
 
       {/* Shift Dialog */}
       <Dialog open={shiftDialogOpen} onOpenChange={setShiftDialogOpen}>
-        <DialogContent className="max-w-lg">
+        <DialogContent className="max-w-lg max-h-[90vh] overflow-y-auto">
           <DialogHeader>
             <DialogTitle>{editingShift ? 'シフト編集' : '新規シフト作成'}</DialogTitle>
           </DialogHeader>
