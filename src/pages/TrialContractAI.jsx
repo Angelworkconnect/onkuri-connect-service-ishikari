0adb85d259afcd9de1d63c68880ac7186212fef1
木村善幸 <kyodajapan@gmail.com>
2026-03-16 03:19:34 +0000

    File changes

---
+++ src/pages/TrialContractAI.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrialToContractAI from '@/components/analytics/TrialToContractAI';
+
+export default function TrialContractAIPage() {
+  return (
+    <div className="min-h-screen bg-slate-50 pb-20">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 py-8">
+        <TrialToContractAI />
+      </div>
+    </div>
+  );
+}
